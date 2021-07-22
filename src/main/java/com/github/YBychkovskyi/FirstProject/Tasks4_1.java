cdd286db5c8ae42cf3cd89e57e4ba2d5d39f89e0
Yaroslav Bychkovskyi <yaroslav1208@gmaail.com>
2021-07-22 16:54:38 +0300

    Задачі 4

---
+++ src/main/java/com/github/YBychkovskyi/FirstProject/Tasks4_1.java
@@ -0,0 +1,12 @@
+package com.github.YBychkovskyi.FirstProject;
+
+import java.util.Scanner;
+
+public class Tasks4_1 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+
+
+  }
+}
