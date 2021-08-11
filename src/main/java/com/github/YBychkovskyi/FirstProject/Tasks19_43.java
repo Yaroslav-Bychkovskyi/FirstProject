2bdd21e8595ccebf9e2791a9bcef915624c22632
Yaroslav-Bychkovskyi <yaroslav1208@gmail.com>
2021-08-11 21:23:11 +0300

    Задачі 17

--- src/main/java/com/github/YBychkovskyi/FirstProject/Tasks19_43.java
+++ src/main/java/com/github/YBychkovskyi/FirstProject/Tasks19_43.java
@@ -1,9 +1,31 @@
 package com.github.YBychkovskyi.FirstProject;
 
+import java.util.Arrays;
+
 public class Tasks19_43 {
-  public static int[] noRepetitionsValues(){
+  public static int[] noRepetitionsValues(int[] masX, int[] masY) {
+    Arrays.sort(masX);
+    Arrays.sort(masY);
+
+    Tasks19_42.ascendingOrder(masX, masY);
+
     return null;
   }
+
+  public static int[] removeElement(int[] m) {
+    for (int i = 0; i < m.length; i++) {
+      for (int j = i + 1; j < m.length; j++) {
+
+        if (m[i] == m[j]) {
+
+
+        }
+      }
+
+    }
+    return null;
+  }
+
 
   public static void main(String[] args) {
     int x = 10;
@@ -15,3 +37,4 @@ public class Tasks19_43 {
     Tasks19_22.creatingArray(massY, x, y);
   }
 }
+
