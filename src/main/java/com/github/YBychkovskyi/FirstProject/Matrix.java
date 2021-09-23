d112c016f4558d70e2feb5ff5e33365a1fa2505b
Yaroslav Bychkovskyi <yaroslav1208@gmail.com>
2021-09-23 16:55:04 +0300

    Задачі 34

---
+++ src/main/java/com/github/YBychkovskyi/FirstProject/Matrix.java
@@ -0,0 +1,9 @@
+package com.github.YBychkovskyi.FirstProject;
+
+public class Matrix {
+  int x = 3;
+  int y = 3;
+  int[][] material = new int[x][y];
+
+
+}
---
+++ src/main/java/com/github/YBychkovskyi/FirstProject/Person.java
@@ -0,0 +1,27 @@
+package com.github.YBychkovskyi.FirstProject;
+
+public class Person {
+  String fullName;
+  int age;
+
+  public Person() {
+  }
+
+  public Person(String fullName, int age) {
+    this.fullName = fullName;
+    this.age = age;
+  }
+
+  public void move() {
+    System.out.println("Такой-то  Person говорит");
+  }
+
+  public void talk() {
+    System.out.println("Такой-то  Person говорит");
+  }
+
+  public static void main(String[] args) {
+    Person person = new Person("Name",20);
+    Person person1 = new Person();
+  }
+}
